af8be24db3d7cbc47c64e98f00f2a0d723800db4
ellaqezi <ellaqezi@gmail.com>
2014-01-30 17:46:22 +0100

    wrote rankhospital.R

---
+++ rankhospital.R
@@ -0,0 +1,30 @@
+rankhospital <- function(state, outcome, num = "best") {
+  ## Read outcome data
+  data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  ## Check that state and outcome are valid
+  stateData <- data[data$State == state,]
+  if (length(stateData[,1]) > 0) {
+    if (outcome == "heart attack") {
+      index = 11      
+    } else if (outcome == "heart failure") {
+      index = 17  
+    } else if (outcome == "pneumonia") {
+      index = 23     
+    } else {
+      stop("invalid outcome")
+    }
+    death <- suppressWarnings(as.numeric(stateData[,index]))
+  } else {
+    stop("invalid state")
+  }
+  ## Return hospital name in that state with the given rank
+  ## 30-day death rate
+  hospital <- stateData[order(death,stateData$Hospital.Name,na.last=NA),2]
+  if(num=="best") {
+    num=1
+  } else if (num=="worst") {
+    num=length(hospital)
+  }
+#  order(rank(death,na.last=T,ties.method="min"))
+  return(hospital[num])
+}
